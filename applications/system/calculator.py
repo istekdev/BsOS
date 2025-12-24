6a6ee1379d21e98207b382b30dd399ded7b54753
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-12-23 22:38:43 -0500

    Update calculator.py

--- applications/system/calculator.py
+++ applications/system/calculator.py
@@ -1,10 +1,32 @@
 from applications.background import appearance
-import customtkinter, math, json, random
-from termcolor import colored
-from core import display
+import customtkinter, json, time
 
 with open("./config.json", "r") as r:
   config = json.load(r)
+
+fs = False
+original = {}
+
+def close():
+    CALC.destroy()
+
+def fullscreen(obj):
+  global fs, original
+  if fs == False:
+    original["x"] = obj.winfo_x()
+    original["y"] = obj.winfo_y()
+    original["w"] = obj.winfo_width()
+    original["h"] = obj.winfo_height()
+    obj.place(x=0, y=0, relwidth=1, relheight=1)
+    obj.configure(corner_radius=0)
+    fs = True
+  else:
+    obj.place(x=original["x"], y=original["y"])
+    obj.configure(width=original["w"], height=original["h"], corner_radius=20)
+    fs = False
+
+def minimize():
+    CALC.iconify()
 
 calcBtns = [
   "(", ")", "C", "/",
@@ -14,26 +36,41 @@ calcBtns = [
   ".", "0", "+/-", "="
 ]
 
-window = customtkinter.CTk()
+CALC = customtkinter.CTk()
 if config["settings"]["appearances"]["theme"] == "light":
-  calculator = customtkinter.CTkFrame(window, width=330, height=470, fg_color=config["settings"]["appearances"]["themes"]["light"]["background"], corner_radius=20)
+  calculator = customtkinter.CTkFrame(CALC, width=330, height=470, fg_color=config["settings"]["appearances"]["themes"]["light"]["background"], corner_radius=20)
   calculator.place(x=200, y=60)
-  header = customtkinter.CTkFrame(calculator, height=40, fg_color=config["settings"]["appearances"]["themes"]["light"]["card"], corner_radius=20)
+  header = customtkinter.CTkFrame(calculator, height=100, fg_color=config["settings"]["appearances"]["themes"]["light"]["card"])
   header.pack(fill="x")
-  customtkinter.CTkLabel(header, text="Calculator", text_color=config["settings"]["appearances"]["themes"]["light"]["title"], font=(config["settings"]["appearances"]["font"], 14, "bold"))
+  title = customtkinter.CTkLabel(header, text="Calculator", text_color=config["settings"]["appearances"]["themes"]["light"]["title"], font=(config["settings"]["appearances"]["font"], 14, "bold"))
+  title.pack(side="right", padx=10)
 elif config["settings"]["appearances"]["theme"] == "dark":
-  calculator = customtkinter.CTkFrame(window, width=330, height=470, fg_color=config["settings"]["appearances"]["themes"]["dark"]["background"], corner_radius=20)
+  calculator = customtkinter.CTkFrame(CALC, width=330, height=470, fg_color=config["settings"]["appearances"]["themes"]["dark"]["background"], corner_radius=20)
   calculator.place(x=200, y=60)
-  header = customtkinter.CTkFrame(calculator, height=40, fg_color=config["settings"]["appearances"]["themes"]["dark"]["card"], corner_radius=20)
+  header = customtkinter.CTkFrame(calculator, height=100, fg_color=config["settings"]["appearances"]["themes"]["dark"]["card"])
   header.pack(fill="x")
-  customtkinter.CTkLabel(header, text="Calculator", text_color=config["settings"]["appearances"]["themes"]["dark"]["title"], font=(config["settings"]["appearances"]["font"], 14, "bold"))
+  title = customtkinter.CTkLabel(header, text="Calculator", text_color=config["settings"]["appearances"]["themes"]["dark"]["title"], font=(config["settings"]["appearances"]["font"], 14, "bold"))
+  title.pack(side="right", padx=10)
 elif config["settings"]["appearances"]["theme"] == "cursed":
-  calculator = customtkinter.CTkFrame(window, width=330, height=470, fg_color=appearance.cursed(), corner_radius=20)
+  calculator = customtkinter.CTkFrame(CALC, width=330, height=470, fg_color=appearance.cursed(), corner_radius=20)
   calculator.place(x=200, y=60)
-  header = customtkinter.CTkFrame(calculator, height=40, fg_color=appearance.cursed(), corner_radius=20)
+  header = customtkinter.CTkFrame(calculator, height=100, fg_color=appearance.cursed())
   header.pack(fill="x")
-  customtkinter.CTkLabel(header, text="Calculator", text_color=appearance.cursed(), font=(config["settings"]["appearances"]["font"], 14, "bold"))
+  title = customtkinter.CTkLabel(header, text="Calculator", text_color=appearance.cursed(), font=(config["settings"]["appearances"]["font"], 14, "bold"))
+  title.pack(side="right", padx=10)
 
+left = customtkinter.CTkFrame(header, fg_color="transparent")
+left.pack(side="left", padx=10)
+
+close = customtkinter.CTkButton(left, text="", width=15, height=15, corner_radius=15, fg_color=config["system"]["controls"]["exit"], command=close)
+minimum = customtkinter.CTkButton(left, text="", width=15, height=15, corner_radius=15, fg_color=config["system"]["controls"]["minimize"], command=minimize)
+maximum = customtkinter.CTkButton(left, text="", width=15, height=15, corner_radius=15, fg_color=config["system"]["controls"]["maximize"], command=lambda: fullscreen(calculator))
+close.pack(side="left", padx=4)
+minimum.pack(side="left", padx=4)
+maximum.pack(side="left", padx=4)
+
+body = customtkinter.CTkFrame(calculator, fg_color="transparent")
+body.pack(fill="both", expand=True, padx=15, pady=15)
 output = customtkinter.StringVar()
 if config["settings"]["appearances"]["theme"] == "light":
   screen = customtkinter.CTkLabel(body, textvariable=output, width=310, height=50, fg_color=config["settings"]["appearances"]["themes"]["light"]["card"], corner_radius=10, anchor="e", font=("Arial", 20))
@@ -41,42 +78,45 @@ elif config["settings"]["appearances"]["theme"] == "dark":
   screen = customtkinter.CTkLabel(body, textvariable=output, width=310, height=50, fg_color=config["settings"]["appearances"]["themes"]["dark"]["card"], corner_radius=10, anchor="e", font=("Arial", 20))
 elif config["settings"]["appearances"]["theme"] == "cursed":
   screen = customtkinter.CTkLabel(body, textvariable=output, width=310, height=50, fg_color=appearance.cursed(), corner_radius=10, anchor="e", font=("Arial", 20))
-screen.pack(pady=(10, 20))
+screen.pack(fill="x", pady=(10, 20))
 
 rows = 0
 columns = 0
-body = customtkinter.CTkFrame(calculator, fg_color="transparent")
-body.pack(fill="both", expand=True, padx=15, pady=15)
 btns = customtkinter.CTkFrame(body, fg_color="transparent")
 btns.pack(expand=True)
 for btn in calcBtns:
   if config["settings"]["appearances"]["theme"] == "light":
-    btn = customtkinter.CTkButton(btns, text=btn, width=60, height=60, corner_radius=15, hover_color=config["settings"]["appearances"]["themes"]["light"]["card2"], fg_color=config["settings"]["appearances"]["themes"]["light"]["card"], text_color=config["settings"]["appearances"]["themes"]["light"]["title"], command=lambda x=btn: calc(x))
+    bt = customtkinter.CTkButton(btns, text=btn, width=60, height=60, corner_radius=15, hover_color=config["settings"]["appearances"]["themes"]["light"]["card2"], fg_color=config["settings"]["appearances"]["themes"]["light"]["card"], text_color=config["settings"]["appearances"]["themes"]["light"]["title"], command=lambda x=btn: calc(x))
   elif config["settings"]["appearances"]["theme"] == "dark":
-    btn = customtkinter.CTkButton(btns, text=btn, width=60, height=60, corner_radius=15, hover_color=config["settings"]["appearances"]["themes"]["dark"]["card2"], fg_color=config["settings"]["appearances"]["themes"]["dark"]["card"], text_color=config["settings"]["appearances"]["themes"]["dark"]["title"], command=lambda x=btn: calc(x))
+    bt = customtkinter.CTkButton(btns, text=btn, width=60, height=60, corner_radius=15, hover_color=config["settings"]["appearances"]["themes"]["dark"]["card2"], fg_color=config["settings"]["appearances"]["themes"]["dark"]["card"], text_color=config["settings"]["appearances"]["themes"]["dark"]["title"], command=lambda x=btn: calc(x))
   elif config["settings"]["appearances"]["theme"] == "cursed":
-    btn = customtkinter.CTkButton(btns, text=btn, width=60, height=60, corner_radius=15, hover_color=appearance.cursed(), fg_color=appearance.cursed(), text_color=appearance.cursed(), command=lambda x=btn: calc(x))
-  btn.grid(row=row, column=columns, padx=6, pady=6)
+    bt = customtkinter.CTkButton(btns, text=btn, width=60, height=60, corner_radius=15, hover_color=appearance.cursed(), fg_color=appearance.cursed(), text_color=appearance.cursed(), command=lambda x=btn: calc(x))
+  bt.grid(row=rows, column=columns, padx=6, pady=6, sticky="nsew")
   columns += 1
   if columns > 3:
-      columns = 0
-      row += 1
+    columns = 0
+    rows += 1
+for b in range(4):
+  btns.grid_columnconfigure(b, weight=1)
+  btns.grid_rowconfigure(b, weight=1)
 
 def calc(val):
   active = output.get()
   if val == "C":
-      output.set("")
+    output.set("")
   elif val == "=":
     try:
-        result = str(eval(active))
-        output.set(result)
-      except Exception:
-          output.set("Error")
+      result = str(eval(active))
+      output.set(result)
+    except Exception:
+      output.set("Error")
+      time.sleep(1)
+      output.set("")
   elif val == "+/-":
     if active:
-        if active.startswith("-"):
-            output.set(active[1:])
-        else:
-            output.set("-" + active)
+      if active.startswith("-"):
+        output.set(active[1:])
+      else:
+        output.set("-" + active)
   else:
     output.set(active + val)
