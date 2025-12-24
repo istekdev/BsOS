5f451c21eaef3ef71d43790fb974515dc96011ce
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-12-24 17:44:19 -0500

    Create cards.py

---
+++ applications/secondary/cards.py
@@ -0,0 +1,12 @@
+import json, time, os, shutil
+from pathlib import Path
+
+with open("./config.json", "r") as r:
+  config = json.load(r)
+
+def install():
+  os.makedirs(f"/local/users/{config["system"]["loggedIn"]}/apps/cards", exist_ok=True)
+  shutil.copytree(f"/external/applications/cards", f"/local/users/{config["system"]["loggedIn"]}/apps/cards", dirs_exist_ok=True)
+
+def uninstall():
+  shutil.rmtree(f"/local/users/{config["system"]["loggedIn"]}/apps/cards")
