5c0ebed127686a7c05bc7f26720690eaf17fa7c4
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-12-26 20:28:32 -0500

    Update meta.py

--- external/applications/cryptocurrency/meta.py
+++ external/applications/cryptocurrency/meta.py
@@ -41,3 +41,19 @@ def upd():
         return "No Need"
   else:
     return "metadata.json Unavailable"
+
+def weight():
+  with open("metadata.json", "r") as r:
+    metadata = json.load(r)
+  weights = []
+  for files in os.listdir(os.path.dirname(os.path.abspath(__file__))):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), files)
+    if os.path.isfile(path):
+      weights.append(os.path.getsize(path))
+  weights = sum(weights)
+  metadata["weight"] = weights
+  
+  static = (metadata["name"].encode("utf-8") + metadata["version"].to_bytes((metadata["version"].bit_length() + 7) // 8, "big") + metadata["created"].to_bytes((metadata["created"].bit_length() + 7) // 8, "big") + metadata["directory"].encode("utf-8") + metadata["update"].encode("utf-8") + weight.to_bytes((weight.bit_length() + 7) // 8, "big"))
+  metadata["verifyHash"] = sha256(static).hexdigest()
+  with open("metadata.json", "w") as w:
+    json.dump(metadata, w, indent=4)
